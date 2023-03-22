ae4901a3d063b1cfc14c3c4dbfed51421ac96c93
Tô Việt Hoàng <totranhieu@gmail.com>
2023-03-22 15:27:47 +0700

    aa

---
+++ src/MyGenericStack.java
@@ -0,0 +1,65 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+import static javafx.beans.binding.Bindings.isEmpty;
+
+public class MyGenericStack<E> {
+    private LinkedList<E> stack;
+    public MyGenericStack(){
+        stack =new LinkedList();
+    }
+
+    public void push(E element) {
+        stack.addFirst(element);
+    }
+    public E pop(){
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+    public int size() {
+        return stack.size();
+    }
+
+    static void stackOfIStrings() {
+        MyGenericStack<String> stack = new MyGenericStack();
+        stack.push("Five");
+        stack.push("Four");
+        stack.push("Three");
+        stack.push("Two");
+        stack.push("One");
+        System.out.println("1.1 Size of stack after push operations: " + stack.size());
+        System.out.printf("1.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %s", stack.pop());
+        }
+        System.out.println("\n1.3. Size of stack after pop operations : " + stack.size());
+    }
+    static void stackOfIntegers() {
+        MyGenericStack<Integer> stack = new MyGenericStack();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("2.1. Size of stack after push operations: " + stack.size());
+        System.out.printf("2.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %d", stack.pop());
+        }
+        System.out.println("\n3.3 Size of stack after pop operations : " + stack.size());
+    }
+
+
+}
+
+
